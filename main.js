e8426d0a573218238b0506b6213b35cd6d235e84
Zalcab08 <78176670+Zalcab08@users.noreply.github.com>
2021-01-30 14:37:00 +0100

    First Version of the webpage uploaded

---
+++ main.js
@@ -0,0 +1,9 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show")
+});
+
+ScrollReveal().reveal('.showcase', { delay: 350 }); 
+ScrollReveal().reveal('.news-card', { delay: 500 }); 
+ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
+ScrollReveal().reveal('.footers-link', { delay: 500 });
+ScrollReveal().reveal('.footer', { delay: 300 });
